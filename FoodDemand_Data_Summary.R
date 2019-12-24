86c408a9fd2d1e2e3cfdb63f373c1c1057f6b68d
Rutvij <44467789+RutvijBhutaiya@users.noreply.github.com>
2019-12-24 14:06:09 +0530

    Updated

--- FoodDemand_Data_Summary.R
+++ FoodDemand_Data_Summary.R
@@ -11,3 +11,66 @@ colSums(is.na(train))  ## No Missing values
 summary(train)
 
 str(train)
+
+attach(train)
+
+## Sort is IMP for Week
+
+train = train[order(week),]
+
+## Remove ID and X vars
+
+train = train[, -c(1,4)]
+
+attach(train)
+
+## Chcke for Outliers Using BoxPlot
+
+boxplot(checkout_price, main = 'CheckOut Price', col = 'salmon')
+
+boxplot(base_price, main = 'BAse Price', col = 'salmon')
+
+boxplot(op_area, main = 'Opt Area Size', col = 'salmon')
+
+
+## Ratio for Class vars
+
+as.matrix(prop.table(table(homepage_featured)))*100
+
+# [,1]
+# 0 89.08001
+# 1 10.91999
+
+as.matrix(prop.table(table(emailer_for_promotion)))*100
+
+# [,1]
+# 0 91.884753
+# 1  8.115247
+
+
+## GGPLot Play
+
+library(ggplot2)
+
+ggplot(train, aes(center_type, num_orders), color = center_type) + 
+  geom_boxplot(aes(fill = center_type))
+
+ggplot(train, aes(cuisine, num_orders), color = cuisine) + 
+  geom_boxplot(aes(fill = cuisine))
+
+ggplot(train, aes(category, num_orders), color = category) + 
+  geom_boxplot(aes(fill = category))
+
+
+
+## NOrmalization
+
+hist(center_id, main = 'Center ID Distribution', col = 'forestgreen')
+hist(meal_id, main = 'Meal ID Distribution', col = 'forestgreen')
+hist(checkout_price, main = 'CheckOut Price Distribution', col = 'forestgreen')
+hist(base_price, main = 'Base Price Distribution', col = 'forestgreen')
+hist(op_area, main = 'Ope Area Distribution', col = 'forestgreen')
+
+
+
+
